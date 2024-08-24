c3118c80ad986015b3f688b61724930ecf122820
tonitaga <gubaydullin.nurislam@gmail.com>
2024-08-24 16:57:31 +0300

    [url-shortener] Creating config file and config struct in golang

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
 
+	"github.com/tonitaga/url-shortener/internal/app/config"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config", "configs/config.yaml", "path to YAML config file")
+}
+
+func main() {
+	flag.Parse()
+
+	// Get configurations from file
+	config := config.NewDefault()
+	if err := config.LoadFromYaml(configPath); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ internal/app/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-yaml/yaml"
+)
+
+type application_config struct {
+	Environment string `yaml:"environment"`
+	Host        string `yaml:"host"`
+	Port        int    `yaml:"port"`
+}
+
+type database_config struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Name     string `yaml:"name"`
+}
+
+type Config struct {
+	Application application_config `yaml:"application"`
+	Database    database_config    `yaml:"database"`
+}
+
+func NewDefault() *Config {
+	return &Config{
+		Application: application_config{
+			Environment: "local",
+			Host:        "localhost",
+			Port:        8080,
+		},
+		Database: database_config{
+			Host:     "localhost",
+			Port:     5432,
+			Username: "postgres",
+			Password: "postgres",
+			Name:     "postgres",
+		},
+	}
+}
+
+func (c *Config) LoadFromYaml(path string) error {
+	const op = "config.Config.LoadFromYaml"
+
+	file, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("%s: %v", op, err)
+	}
+	defer file.Close()
+
+	if err := yaml.NewDecoder(file).Decode(c); err != nil {
+		return fmt.Errorf("%s: %v", op, err)
+	}
+
+	return nil
+}
